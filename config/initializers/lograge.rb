0fdd311d2a1a94a2770d09f8d73982d2b96f27a9
Iwan Buetti <iwan.buetti@gmail.com>
2019-12-03 11:57:01 +0100

    Add lograge output

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -14,4 +14,14 @@ Rails.application.configure do
   end
 
   config.lograge.formatter = Lograge::Formatters::Logstash.new
+
+
+  config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
 end
+
+# {"method":"POST","path":"/books","format":"html","controller":"BooksController",
+#   "action":"create","status":302,"duration":62.47,"view":0.0,"db":9.86,
+#   "location":"http://localhost:3000/books/5","time":"2019-12-03T00:04:58.739+01:00",
+#   "host":"localhost","username":"Susanna Verdi",
+#   "@timestamp":"2019-12-02T23:04:58.739Z","@version":"1",
+#   "message":"[302] POST /books (BooksController#create)"}
